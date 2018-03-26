b64f2007d98f0f94a7a9e4df737831a400b9ebd6
Ionut Emanuel Mihailescu <emiionut@gmail.com>
2018-03-26 12:15:26 +0300

    Working query

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {MatCardModule, MatGridListModule} from "@angular/material";
 
 
 @NgModule({
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
