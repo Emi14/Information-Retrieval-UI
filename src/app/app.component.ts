c7c716b33e63c060f18354fdef41631967cfbac6
Ionut Emanuel Mihailescu <emiionut@gmail.com>
2018-03-26 12:15:26 +0300

    Working query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Information Retrieval UI';
+  query: string;
+  apiUrl= 'http://localhost:8080/api/search';
+  singleQueryUrl= '/singleQuery?query=';
+  status: string;
+  resultCount = 0;
+  documents = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  callApi() {
+    if (this.query == null) {
+      this.status = 'emptyQuery';
+    } else {
+      this.http.get(this.apiUrl + this.singleQueryUrl + this.query).subscribe(response => {
+        this.status = response['searchDetails']['status'];
+        this.resultCount = response['searchDetails']['resultsCount'];
+        this.documents = response['searchResults'];
+        console.log(this.documents);
+      })
+    }
+  }
+
 }
